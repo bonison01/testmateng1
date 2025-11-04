48a88bd6a6215114e823e367437caea127d027fd
bonison01 <bonisonkhumbongmayum@gmail.com>
2025-11-04 19:01:30 +0530

    dsf

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -25,7 +25,7 @@ export default function Footer() {
         </Link>
 
         {/* Dropup Toggle Button */}
-        <div className="relative">
+        {/* <div className="relative">
           <Button
             variant="link"
             className='p-0 h-6 text-zinc-500 hover:text-green-600'
@@ -35,7 +35,7 @@ export default function Footer() {
           </Button>
 
           {/* Dropup Content */}
-          {/* {open && (
+          {open && (
             <div className="absolute bottom-full mb-2 right-0 bg-white border border-zinc-200 rounded-md shadow-lg py-2 z-50 w-48">
               <Link href="https://invoice-one-snowy.vercel.app">
                 <Button variant="link" className='w-full text-left px-4 py-1.5 text-zinc-500 hover:text-green-600'>
@@ -53,8 +53,8 @@ export default function Footer() {
                 </Button>
               </Link>
             </div>
-          )} */}
-        </div>
+          )}
+        </div> */}
       </div>
     </div>
   );
